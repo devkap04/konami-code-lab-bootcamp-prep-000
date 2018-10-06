03d082095333fac70623277f56ea9f3d464fca3f
devinkapelanski <devinkapelanski@yahoo.com>
2018-10-06 11:01:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,7 +12,7 @@ const codes = [
 ];
 
 function init() {
-  let index = 0;
+  var index = 0;
   document.addEventListener('keydown', onKeyDownHandler);
   function onKeyDownHandler(e) {
     const key = e.key;
